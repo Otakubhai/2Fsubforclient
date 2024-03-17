805444ad0326aa7cd3a1368b343b929cde23ddf9
Bhargav Gadekar <87870903+Otakubhai@users.noreply.github.com>
2024-03-17 13:41:59 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Sabka Malik : <a href='tg://user?id={OWNER_ID}'>@MV_Streamer</a>\n○ Check Out : @WebCinematics\n○ Channel : @Otaku_Library \n</b>",
+            text = f"<b>○ Sabka Malik : <a href='tg://user?id={OWNER_ID}'>@MV_Streamer</a>\n○ Check Out : @WebCinematics\n❤️ Made By : @TheBotsLibrary \n</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
